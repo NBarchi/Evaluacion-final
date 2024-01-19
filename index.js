786e6d9236719a796c9371d9b98242cc9d2ea2ce
NBarchi <nicobarrera.g10@gmail.com>
2024-01-18 19:59:15 -0500

    Evaluación Final

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let cambiarColor=()=>{
+    const bloque2=document.getElementById("bloque2");
+
+    bloque2.style.backgroundColor='red'
+}
+
+let bloque1=document.getElementById("bloque1");
+
+bloque1.onmouseover=cambiarColor;
+
+
